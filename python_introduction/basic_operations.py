e6509371d7a387ebcec9c2932ce7d5189604af62
Owagbemi Olumide <olumideowagbemi@gmail.com>
2024-09-13 03:12:54 +0100

    basic operations file added

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,4 +1,9 @@
 #!/bin/bash
-Addition of 10 and 5 is 15
-Subtraction of 10 and 5 is 5
-Multiplication of 10 and 5 is 50
+number1 = 10
+number2 = 5
+Addition = number1 + number2
+print(Addition)
+Subtraction = number1 - number2
+print(Subtraction)
+Multiplication = number1 * number2
+print(Multiplication)
