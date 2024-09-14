b9771d1eb41799ef96c51c85ff6807c51d45662a
Owagbemi Olumide <olumideowagbemi@gmail.com>
2024-09-15 00:18:32 +0100

    updated the finance calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
 #!/bin/bash
-monthly_income = input("Enter your monthly income: ")
-total_monthly_expenses = input("Enter your total monthly expenses: ")
-monthly_savings = float(monthly_income) - float(total_monthly_expenses)
-print("Your monthly savings are $" + str(monthly_savings))
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
+print("Your monthly savings are $" + str(int(monthly_savings)) + ".")
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
-print("Projected savings after one year, with interest, is: $" + str(projected_savings))
+print("Projected savings after one year, with interest, is: $" + str(int(projected_savings)) + ".")
