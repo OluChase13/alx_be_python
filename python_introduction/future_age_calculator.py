9acafd3523c3198fdfe4bf078ed20d0ef07b5abf
Owagbemi Olumide <olumideowagbemi@gmail.com>
2024-09-14 09:11:42 +0100

    future age calculator file added

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,5 +1,5 @@
 #!/bin/bash
 current_year = 2023
-current_age = input("What is your age? ")
+current_age = input("How old are you? ")
 age_in_2050 = current_year + int(current_age)
 print(age_in_2050)
