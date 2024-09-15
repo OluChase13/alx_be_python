96ff80d614724e1eecb3f54016f7350ecbc28c0d
Owagbemi Olumide <olumideowagbemi@gmail.com>
2024-09-15 01:29:33 +0100

    updated basic operartions file

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -2,8 +2,8 @@
 number1 = 10
 number2 = 5
 Addition = number1 + number2
-print(Addition)
+print("Addition of " +  str(number1) + " and "+ str(number2) + " is " + str(Addition))
 Subtraction = number1 - number2
-print(Subtraction)
+print("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(Subtraction))
 Multiplication = number1 * number2
-print(Multiplication)
+print("Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(Multiplication))
