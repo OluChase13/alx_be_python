4f51962fdee242e6d74988bf33803a7668ff637f
Owagbemi Olumide <olumideowagbemi@gmail.com>
2024-09-23 00:00:39 +0100

    daily reminder file updated

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,16 +1,16 @@
 #!/bin/bash
-task = input("Enter your task for today: ")
-priority = input("What is the priority level of this task? (high, medium, low): ").lower()
-time_bound = input("Is the task time-sensitive? (yes or no): ").lower()
-match priority:
+Task = input("Enter your task for today: ")
+Priority = input("What is the priority level of this task? (high, medium, low): ").lower()
+Time_bound = input("Is the task time-sensitive? (yes or no): ").lower()
+match Priority:
     case "high":
-        reminder = "Reminder: '" + task + "' is a high priority task."
+        reminder = "Reminder: '" + Task + "' is a high priority task."
     case "medium":
-        reminder = "Reminder: '" + task + "' is a medium priority task."
+        reminder = "Reminder: '" + Task + "' is a medium priority task."
     case "low":
-        reminder = "Reminder: '" + task + "' is a low priority task."
+        reminder = "Reminder: '" + Task + "' is a low priority task."
     case _:
-        reminder = "Reminder: '" + task + "' has an unknown priority."
-if time_bound == "yes":
+        reminder = "Reminder: '" + Task + "' has an unknown priority."
+if Time_bound == "yes":
     reminder += " It requires immediate attention today!"
     print(reminder)
