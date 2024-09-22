f381aa9f0af1ffb29408e9677c5fd4fd407206d9
Owagbemi Olumide <olumideowagbemi@gmail.com>
2024-09-22 22:32:53 +0100

    weather advice file added

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,5 +1,5 @@
 #!/bin/bash
-weather = input("What's the weather today? (sunny/rainy/cold): ").lower()
+weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 if weather=="sunny":
     print("Wear a t-shirt and sunglasses.")
 elif weather=="rainy":
