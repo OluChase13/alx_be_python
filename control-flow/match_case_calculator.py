93ea168a89fa716892f19f478f7ebd9cd62f1950
Owagbemi Olumide <olumideowagbemi@gmail.com>
2024-09-22 23:01:29 +0100

    match case file updated

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,6 +1,6 @@
 #!/bin/bash
 number1 = float(input("Enter the first number: "))
-number2 = float(input("Enter the second name: "))
+number2 = float(input("Enter the second number: "))
 operation = input("Choose the operation (+, -, *, /): ")
 match operation:
     case "+":
