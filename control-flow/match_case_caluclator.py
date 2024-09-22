8ba0c30e2c699621f0ada424f62d03f88360d037
Owagbemi Olumide <olumideowagbemi@gmail.com>
2024-09-22 22:57:00 +0100

    match case file added

---
+++ control-flow/match_case_caluclator.py
@@ -0,0 +1,20 @@
+#!/bin/bash
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second name: "))
+operation = input("Choose the operation (+, -, *, /): ")
+match operation:
+    case "+":
+        result = number1 + number2 
+        print("The result is", result)
+    case "-":
+        result = number1 - number2
+        print("The result is", result)
+    case "*":
+        result = number1 * number2
+        print("The result is", result)
+    case "/":
+        if number2 != 0:
+            result = number1 / number2
+            print("The result is", result)
+        else:
+            print("Cannot divide by zero.")
