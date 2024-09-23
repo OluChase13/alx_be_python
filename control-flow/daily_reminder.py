5ccf50ead43db98f1baf0bb7302e6a2b17946e43
Owagbemi Olumide <olumideowagbemi@gmail.com>
2024-09-24 00:10:10 +0100

    daily reminder file updated

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -5,21 +5,21 @@ Time_bound = input("Is the task time-sensitive? (yes or no): ").strip().lower()
 yes_time = "that requires immediate attention today!"
 no_time = "consider completing when you have time"
 
-match priority:
+match Priority:
     case "high":
         if Time_bound == "yes":
-            print(f"Reminder: '{Task}' is a {Priority} priority task {yes_time}")
+            print(f"Reminder: '{Task}' is a {Priority} priority task{yes_time}")
         elif Time_bound == "no":
-            print(f"Reminder: '{Task}' is a {Priority} priority task {no_time}")
+            print(f"Reminder: '{Task}' is a {Priority} priority task{no_time}")
     
     case "medium":
         if Time_bound == "yes":
-            print(f"Attention: '{Task}' is a {Priority} priority task {yes_time}")
+            print(f"Attention: '{Task}' is a {Priority} priority task{yes_time}")
         elif Time_bound == "no":
-            print(f"Attention: '{Task}' is a {Priority} priority task {yes_time}")
+            print(f"Attention: '{Task}' is a {Priority} priority task{yes_time}")
 
     case "low":
         if Time_bound == "yes":
-            print(f"Note: '{Task}' is a {Priority} priority task {yes_time}")
+            print(f"Note: '{Task}' is a {Priority} priority task{yes_time}")
         elif Time_bound == "no":
-            print(f"Note: '{Task}' is a {Priority} priority task {yes_time}")
+            print(f"Note: '{Task}' is a {Priority} priority task{yes_time}")
