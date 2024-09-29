e7265e60dc6cc25aaf4501bcfaf693c1963f33e3
Owagbemi Olumide <olumideowagbemi@gmail.com>
2024-09-29 22:10:29 +0100

    temp conversion tool updated

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,12 +1,11 @@
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
-OFFSET = 32
 
 def convert_to_celsius(fahrenheit):
-    return (fahrenheit - OFFSET) * FAHRENHEIT_TO_CELSIUS_FACTOR 
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR 
 
 def convert_to_fahrenheit(celsius):
-    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + OFFSET
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 temperature_value = (input('Enter the temperature to convert: '))
 if temperature_value.isdigit():
