d7d14a2f810db0934a5d436ea5bb0751997e1bd0
Owagbemi Olumide <olumideowagbemi@gmail.com>
2024-09-29 11:02:11 +0100

    arithmetic file updated

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,9 +7,8 @@ def perform_operation(num1, num2, operation):
     elif operation == "multiply":
         return num1 * num2
     elif operation == "divide":
-        if num2 ==0:
-            return "Error: Division by zero"
-        else:
-            return num1 / num2
+        if num2 == 0:
+            return "Error: Division by zero is not allowed"
+        return num1 / num2
     else:
-        print("Incoorect input")
+        print("Incorrect input. Please use 'add', 'subtract', 'multiply' or 'divide'.")
