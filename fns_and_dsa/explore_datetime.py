47cd96df0a4df659c14b8a90d12566a077ef33d5
Owagbemi Olumide <olumideowagbemi@gmail.com>
2024-09-29 14:35:55 +0100

    datetime file added

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timedelta
+
+def display_current_datetime():
+    current_date = datetime.now()
+    date_format = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print("Current Date and Time:", date_format)
+def calculate_future_date():
+    number_of_days = int(input("Enter number of days: "))
+    current_date = datetime.now()
+    future_date = current_date + timedelta(days="number_of_days")
+    future_date_format = future_date.strftime("%Y-%m-%d")
+    print("Future date after adding", number_of_days, "days:", future_date_format )
+
+def main():
+    display_current_datetime
+    calculate_future_date
+
+if __name__=="_main_":
+    main()
