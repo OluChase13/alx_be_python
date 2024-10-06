cb661a343102b17e3e10dd913bb99fd9f94432bb
Owagbemi Olumide <olumideowagbemi@gmail.com>
2024-10-06 15:37:23 +0100

    reobust division calc file added

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -7,4 +7,4 @@ def safe_divide(numerator, denominator):
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
     except ValueError:
-        return "Error: Non-numeric input. Input a numrical value"
+        return "Error: Please enter numeric values only."
