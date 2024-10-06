4cd05f67b1ee60b91df4ceb30a86613316fc7ac8
Owagbemi Olumide <olumideowagbemi@gmail.com>
2024-10-06 15:35:57 +0100

    reobust division calc file added

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -5,6 +5,6 @@ def safe_divide(numerator, denominator):
         result = numerator / denominator
         return f"The result of {numerator} / {denominator} is {result:.2f}"
     except ZeroDivisionError:
-        return "Error: Division by zero is not allowed"
+        return "Error: Cannot divide by zero."
     except ValueError:
         return "Error: Non-numeric input. Input a numrical value"
